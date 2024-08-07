b5bdbd5f1a2899d5d192ac76c0206d086b181db8
Adophilus <uchenna19of@gmail.com>
2024-08-07 21:46:27 +0100

    ready to push to github pages

--- simple-pwa/sw.js
+++ simple-pwa/sw.js
@@ -1,3 +1,15 @@
-self.addEventListener("install", () => {
-  console.log("Installed")
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/assets/scripts/index.js',
+  '/assets/scripts/components/counter.js',
+  '/assets/styles/index.css',
+  '/assets/styles/reset.css',
+]
+const CACHE_KEY = 'v0.0.1'
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_KEY).then(cache => {
+    cache.addAll(STATIC_ASSETS)
+  }))
 })
