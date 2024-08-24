9265e3fb7f60967f33c320f7907d5a0ca3a3b651
Adophilus <uchenna19of@gmail.com>
2024-08-24 13:43:13 +0100

    updated the code for the checkbox

---
+++ custom-checkbox/src/components/checkbox.ts
@@ -0,0 +1,35 @@
+import styles from "./checkbox.module.css?raw";
+
+export class MyCheckbox extends HTMLElement {
+	private declare checkboxEl: HTMLInputElement;
+
+	constructor() {
+		super();
+
+		const id = Date.now().toString();
+
+		const shadowRoot = this.attachShadow({ mode: "open" });
+
+		this.checkboxEl = document.createElement("input");
+		this.checkboxEl.setAttribute("type", "checkbox");
+		this.checkboxEl.setAttribute("id", id);
+		shadowRoot.appendChild(this.checkboxEl);
+
+		const labelEl = document.createElement("label");
+		labelEl.setAttribute("for", id);
+		labelEl.appendChild(document.createElement("slot"));
+		shadowRoot.appendChild(labelEl);
+
+		const sheet = new CSSStyleSheet();
+		sheet.replaceSync(styles);
+		shadowRoot.adoptedStyleSheets = [sheet];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.checkboxEl.setAttribute("id", newValue);
+	}
+
+	connectedCallback() {}
+}
+
+window.customElements.define("my-checkbox", MyCheckbox);
