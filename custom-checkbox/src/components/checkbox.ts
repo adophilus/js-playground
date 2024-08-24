fc3a772c1fcd503f811cd53432698a854d4eab72
Adophilus <uchenna19of@gmail.com>
2024-08-24 21:46:43 +0100

    done with the new checkbox

--- custom-checkbox/src/components/checkbox.ts
+++ custom-checkbox/src/components/checkbox.ts
@@ -3,6 +3,8 @@ import checkmark from "./checkmark.svg?raw";
 
 export class MyCheckbox extends HTMLElement {
 	private declare checkboxEl: HTMLInputElement;
+	private declare btnEl: HTMLButtonElement;
+	private declare svgEl: SVGElement;
 
 	constructor() {
 		super();
@@ -14,14 +16,29 @@ export class MyCheckbox extends HTMLElement {
 		this.checkboxEl = document.createElement("input");
 		this.checkboxEl.setAttribute("type", "checkbox");
 		this.checkboxEl.setAttribute("id", id);
+		this.checkboxEl.addEventListener("change", async () => {
+			switch (this.checkboxEl.checked) {
+				case true: {
+					this.triggerCheckboxCheckedAnimation();
+					break;
+				}
+				case false: {
+					this.triggerCheckboxUncheckedAnimation();
+					break;
+				}
+			}
+		});
+
 		shadowRoot.appendChild(this.checkboxEl);
 
-		const btnElement = document.createElement("button");
-		btnElement.addEventListener("click", () => {
-			this.checkboxEl.checked = !this.checkboxEl.checked;
+		this.btnEl = document.createElement("button");
+		this.btnEl.addEventListener("click", () => {
+			this.checkboxEl.click();
 		});
-		btnElement.innerHTML = checkmark;
-		shadowRoot.appendChild(btnElement);
+		this.btnEl.innerHTML = checkmark;
+		shadowRoot.appendChild(this.btnEl);
+		// biome-ignore lint/style/noNonNullAssertion: this is fine
+		this.svgEl = this.btnEl.querySelector<SVGElement>("svg")!;
 
 		const labelEl = document.createElement("label");
 		labelEl.setAttribute("for", id);
@@ -39,7 +56,62 @@ export class MyCheckbox extends HTMLElement {
 		}
 	}
 
-	connectedCallback() {}
+	private async triggerCheckboxCheckedAnimation() {
+		this.btnEl.animate(
+			[
+				{
+					transform: "scale(100%) rotate(0deg)",
+					boxShadow: "0px 0px 0px 0px rgba(164, 199, 246, 0)",
+				},
+				{
+					transform: "scale(120%) rotate(20deg)",
+					boxShadow: "0px 0px 1px 6px rgba(164, 199, 246, 1)",
+				},
+				{
+					transform: "scale(100%) rotate(0deg)",
+					boxShadow: "0px 0px 0px 0px rgba(164, 199, 246, 0)",
+				},
+			],
+			{
+				fill: "forwards",
+				duration: 250,
+				easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+			},
+		);
+		this.svgEl.animate(
+			[
+				{
+					strokeDashoffset: "-23",
+				},
+				{
+					strokeDashoffset: "0",
+				},
+			],
+			{
+				fill: "forwards",
+				duration: 250,
+				easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+			},
+		);
+	}
+
+	private async triggerCheckboxUncheckedAnimation() {
+		this.svgEl.animate(
+			[
+				{
+					strokeDashoffset: "0",
+				},
+				{
+					strokeDashoffset: "-23",
+				},
+			],
+			{
+				fill: "forwards",
+				duration: 250,
+				easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+			},
+		);
+	}
 }
 
 window.customElements.define("my-checkbox", MyCheckbox);
