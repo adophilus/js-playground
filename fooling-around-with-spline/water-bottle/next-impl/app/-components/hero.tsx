b480b3cd20e426e4dab402b32079cc6a9ad9b3d4
Adophilus <uchenna19of@gmail.com>
2024-08-26 11:24:41 +0100

    updated the repo

---
+++ fooling-around-with-spline/water-bottle/next-impl/app/-components/hero.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Spline from "@splinetool/react-spline/next";
+import { useEffect, useRef } from "react";
+
+const sleep = (ms: number) => new Promise((res) => setInterval(res, ms));
+
+export const Hero = () => {
+	const textContainer = useRef<HTMLHeadingElement>(null);
+
+	const text = {
+		leading: "Need a recharge?",
+		lagging: "Have a refill",
+	};
+
+	const beginAnimation = async () => {
+		const container = textContainer.current!;
+
+		for (const letter of text.leading) {
+			const span = document.createElement("span");
+			span.style.transform = "scale(110%)";
+			span.innerText = letter;
+
+			container.appendChild(span);
+			await sleep(500);
+		}
+	};
+
+	useEffect(() => {
+		beginAnimation();
+	}, []);
+
+	return (
+		<div className="h-screen relative">
+			<Spline scene="https://prod.spline.design/3gUaPddC3Uo9NOsz/scene.splinecode" />
+			<div className="absolute inset-0 bg-black/20 pointer-events-none" />
+			<div className="absolute z-20 container inset-0 mx-auto pointer-events-none text-white grid h-full place-items-center">
+				{/* biome-ignore lint/a11y/useHeadingContent: <explanation> */}
+				<h2
+					className="text-white text-[10rem] font-NewAmsterdam tracking-wide text-center"
+					ref={textContainer}
+				>
+					Need a refill?
+				</h2>
+			</div>
+		</div>
+	);
+};
---
+++ fooling-around-with-spline/water-bottle/plain/src/index.ts
@@ -0,0 +1,58 @@
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import "./style.css";
+import three, {
+	Scene,
+	PerspectiveCamera,
+	WebGLRenderer,
+	BoxGeometry,
+	MeshBasicMaterial,
+	Mesh,
+	Camera,
+} from "three";
+
+const scene = new Scene();
+let camera: Camera = new PerspectiveCamera(120, 1920 / 1080, 0.1, 1000);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(1920, 1080);
+document.body.appendChild(renderer.domElement);
+
+// const geometry = new BoxGeometry(1, 1, 1)
+// const material = new MeshBasicMaterial({ color: 0x00ff00 })
+// scene.add(cube)
+
+// const cube = new Mesh(geometry, material)
+// camera.position.z = 5
+
+// camera.position.x = 1938.35
+// camera.position.y = -554.53
+// camera.position.z = 517.46
+//
+// camera.rotation.x = -118.77
+// camera.rotation.y = 85.07
+// camera.rotation.z = 118.86
+//
+// camera.scale.x = 1
+// camera.scale.y = 1
+// camera.scale.z = 2.73
+
+const loader = new GLTFLoader();
+loader.load(
+	"/media/models/bottle.glb",
+	(gltf) => {
+		camera = gltf.cameras.find((camera) => camera.name === "view")!;
+		gltf.scene.children.forEach((child) => {
+			console.log(child);
+			scene.add(child);
+		});
+	},
+	undefined,
+	(err) => console.warn(err),
+);
+
+renderer.setAnimationLoop(() => {
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+});
