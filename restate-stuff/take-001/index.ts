67c4960ee2623746129a48c1afc0e979426f7828
Adophilus <uchenna19of@gmail.com>
2026-06-15 13:04:47 +0100

    chore: checked out restate

---
+++ restate-stuff/take-001/index.ts
@@ -0,0 +1,67 @@
+import * as restate from "@restatedev/restate-sdk";
+import { durableCalls } from "@restatedev/vercel-ai-middleware";
+// import { createOpenAI } from "@ai-sdk/openai";
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible';
+import { generateText, stepCountIs, tool, wrapLanguageModel } from "ai";
+import { z } from "zod";
+
+// TOOL
+async function getWeather(ctx: restate.Context, city: string) {
+  // Do durable steps using the Restate context
+  return ctx.run(`get weather ${city}`, () => {
+    // Simulate calling the weather API
+    return { temperature: 23, description: `Sunny and warm.` };
+  });
+}
+
+// AGENT
+const run = async (ctx: restate.Context, { prompt }: { prompt: string }) => {
+  const openai = createOpenAICompatible({
+    name: 'zai',
+    baseURL: process.env.ZAI_OPENAI_COMPATIBLE_URL!,
+    apiKey: process.env.ZAI_API_KEY,
+  });
+
+  const model = wrapLanguageModel({
+    model: openai("glm-4.5"),
+
+    // Persist LLM responses
+    middleware: durableCalls(ctx, { maxRetryAttempts: 3 }),
+  });
+
+  const { text } = await generateText({
+    model,
+    system: "You are a helpful agent that provides weather updates.",
+    prompt,
+    tools: {
+      getWeather: tool({
+        description: "Get the current weather for a given city.",
+        inputSchema: z.object({ city: z.string() }),
+        execute: async ({ city }) => getWeather(ctx, city),
+      }),
+    },
+    stopWhen: [stepCountIs(5)],
+    providerOptions: { openai: { parallelToolCalls: false } },
+  });
+
+  return text;
+};
+
+// AGENT SERVICE
+const agent = restate.service({
+  name: "agent",
+  handlers: {
+    run: restate.createServiceHandler(
+      {
+        input: restate.serde.schema(
+          z.object({
+            prompt: z.string().default("What's the weather in San Francisco?"),
+          }),
+        ),
+      },
+      run,
+    ),
+  },
+});
+
+restate.serve({ services: [agent] });
