9d1ec83fa7725ae5aebcdfdf6dcd1ba46a1b573c
Adophilus <uchenna19of@gmail.com>
2026-08-15 22:39:01 +0100

    chore: stubbed out a component

--- react-stuff/checking-out-rsd/src/app.tsx
+++ react-stuff/checking-out-rsd/src/app.tsx
@@ -3,7 +3,9 @@ import { tokens } from "./styles/tokens.css";
 import { Header } from "./components/header";
 
 export const App = () => (
-  <>
+  <html.main style={styles.container}>
     <Header />
-  </>
+  </html.main>
 );
+
+const styles = css.create({ container: { backgroundColor: tokens.colorGrey } });
--- react-stuff/checking-out-rsd/src/components/header.tsx
+++ react-stuff/checking-out-rsd/src/components/header.tsx
@@ -1,1 +1,52 @@
-export const Header = () => null;
+import { css, html } from "react-strict-dom";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  BellDotIcon,
+  InformationCircleIcon,
+  Search01Icon,
+} from "@hugeicons/core-free-icons";
+import { tokens } from "@/styles/tokens.css";
+
+const items = [
+  {
+    label: "Overview",
+    link: "/",
+    isActive: true,
+  },
+  { label: "Activity", link: "/activity" },
+  { label: "Manage", link: "/manage" },
+  { label: "Program", link: "/program" },
+  { label: "Account", link: "/account" },
+  { label: "Reports", link: "/reports" },
+];
+
+const extras = [
+  { label: "Search", icon: Search01Icon },
+  { label: "Notifications", icon: BellDotIcon },
+  { label: "Info", icon: InformationCircleIcon },
+];
+
+export const Header = () => (
+  <html.nav>
+    <html.a style={styles.logoContainer}>
+      <html.span></html.span>
+      LOGO
+    </html.a>
+    <html.ul>
+      {items.map((item) => (
+        <html.ol key={item.link}>{item.label}</html.ol>
+      ))}
+    </html.ul>
+    <html.ul>
+      {extras.map((extra) => (
+        <html.ol key={extra.label}>
+          <HugeiconsIcon icon={extra.icon} />
+        </html.ol>
+      ))}
+    </html.ul>
+  </html.nav>
+);
+
+const styles = css.create({
+  logoContainer: { backgroundColor: tokens.colorWhite },
+});
