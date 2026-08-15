9d425aab975d0c6bf9dec177354cbdb0ee9ce041
Adophilus <uchenna19of@gmail.com>
2026-08-15 22:21:34 +0100

    chore: fixed typo in postcss config

--- react-stuff/checking-out-rsd/postcss.config.js
+++ react-stuff/checking-out-rsd/postcss.config.js
@@ -3,7 +3,7 @@ import babelConfig from "./babel.config.js";
 export default {
   plugins: {
     "react-strict-dom/postcss-plugin": {
-      include: ["src/**/*.{js,jsx,mjs,tx,tsx}"],
+      include: ["src/**/*.{js,jsx,mjs,ts,tsx}"],
       babelConfig,
       useLayers: true,
     },
